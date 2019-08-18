3a3dfdb7681d81dc7c910c5a401bf3ea6e6c697e
wangchengzhang <710724443@qq.com>
2019-08-18 23:27:46 +0800

    SSM整合系列-Spring整合SpringMVC v1.0

--- src/main/java/com/waston/controller/AccountController.java
+++ src/main/java/com/waston/controller/AccountController.java
@@ -1,5 +1,7 @@
 package com.waston.controller;
 
+import com.waston.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,9 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/account")
 public class AccountController {
 
+    @Autowired
+    private AccountService accountService;
+
     @RequestMapping("findAll")
     public String findAll(){
         System.out.println("表现层：查询所有账户...");
+        //调用service方法
+        accountService.findAll();
         return "list";
     }
 }
