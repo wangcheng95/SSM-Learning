e20bb6e818e6caca6946802dd431e327f19b02dc
wangchengzhang <710724443@qq.com>
2019-08-18 23:15:21 +0800

    SSM整合系列-Spring MVC 框架的搭建v1.0

--- src/main/java/com/waston/controller/AccountController.java
+++ src/main/java/com/waston/controller/AccountController.java
@@ -1,7 +1,18 @@
 package com.waston.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
- * 编写web
+ * 账号web
  */
+@Controller
+@RequestMapping("/account")
 public class AccountController {
+
+    @RequestMapping("findAll")
+    public String findAll(){
+        System.out.println("表现层：查询所有账户...");
+        return "list";
+    }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,6 +1,8 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <body>
-<h2>文件上传服务器启动成功</h2>
+<h1>SSM整合</h1>
+
+<a href="/account/findAll">测试</a>
 </body>
 </html>
